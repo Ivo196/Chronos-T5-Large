0f8c2f3ab6397581427765939c62205e49806020
Ivo196 <ivo.tonioni@gmail.com>
2025-03-27 17:28:21 +0100

    add forecastin

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ import streamlit as st
 import pandas as pd 
 import matplotlib.pyplot as plt
 import datetime
+import torch
+from chronos import ChronosPipeline
+import numpy as np
 
 
+def predict_chronos_t5(data, prediction_length):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(device)
 
+    pipeline = ChronosPipeline.from_pretrained(
+        'amazon/chronos-t5-tiny',
+        device_map = device,
+        torch_dtype = torch.float32
+    )
+    context = torch.tensor(data=data['Close'], dtype=torch.float32)
+
+    forecast = pipeline.predict(context, prediction_length, num_samples=100,)
+    low, median, high = np.quantile(forecast[0].numpy(), [0.1, 0.5, 0.9], axis=0)
+    return median
 # Title and description 
 st.title('Crypto Price Prediction 🚀')
 st.write('''
@@ -34,3 +50,15 @@ if st.checkbox('Show Historical Data'):
     ax.set_title("Historical Data")
     ax.legend()
     st.pyplot(fig)
+
+
+# Days to forecast 
+prediction_length = st.number_input('Days to predict', min_value=1, max_value=30, value=7, step=1)
+
+# Button to make a prediction and show results
+
+if st.button('Make a pediction'):
+    st.subheader(f'The price for the following {prediction_length} days is ...')
+    prediction = predict_chronos_t5(data,prediction_length)
+    st.write(prediction)
+
